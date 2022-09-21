4cbcaa9cd70718bb4730c80b106c9c387ffa8387
Robin <robin.kriisa@voco.ee>
2022-09-21 10:38:50 +0300

    Moodle yl 1.1-1.5

--- variables&datatypes/app.js
+++ variables&datatypes/app.js
@@ -7,7 +7,61 @@
 console.log("1")
 console.log("2")
 console.log("3")*/
+/*1.1
+console.log("Hello World");
+*/
 
-console.log("Hello World")
-// 1 realine kommentaar
+/*
+1.2
+let aasta = "2021. ";
+let president = "Alar Karis";
+let lause_keskosa = "aastal valiti presidendiks ";
+let lause = (aasta + lause_keskosa + president);
+console.log(lause);
+*/
+
+/*1.3
+let x = prompt("Sisesta väärtus",0 );
+let y = prompt("Sisesta aste",0);
+console.log(Math.pow(x,y));
+*/
+
+/*
+alternatiiv 1.3
+
+const power = (number,n) => {
+    if(n === 0)
+       return 1
+    let result = 1
+    while(n !=0){
+        n--
+        result *= number
+    }
+    return result
+}
+console.log(power(x,y))
+*/
+
+
+
+/*
+1.4
+
+const nimi = prompt("Sisestage oma nimi");
+let lubatud = prompt("Sisestage lubatud kiirus", 0);
+let tegelik = prompt("Sisestage tegelik kiirus",0);
+console.log(nimi + ",","kiiruse ületamise eest on teie trahv", (tegelik - lubatud)*3, "\u20AC");
+*/
+
+/*
+1.5
+let eap = prompt("Sisestage ainepunktide arv");
+let nadal = prompt("Sisestage nädalate arv");
+let ajakulu = (eap * 26 / nadal);
+console.log("Nädala eeldatav ajakulu on", Math.round(ajakulu,0),"h");
+*/
+
+
+
+
 
